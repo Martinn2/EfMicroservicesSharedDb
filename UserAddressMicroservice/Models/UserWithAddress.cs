fb5a726d4da4185d4f199d37e075db4795f97458
Martin Smíšek <masmisek@gmail.com>
2021-07-30 16:47:47 +0200

    UserWithAddress added

---
+++ UserAddressMicroservice/Models/UserWithAddress.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserMicroservice.Models;
+
+namespace UserAddressMicroservice.Models
+{
+
+    //If I need navigation property I would like to use this class
+    //I would change UserAddress to have reference to this class instead of user
+    public class UserWithAddress: User
+    {
+        public UserAddress Address { get; set; }
+    }
+}
